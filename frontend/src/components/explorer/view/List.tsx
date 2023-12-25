ceb0cc03e8d4d3c82aa157da9c73cb5f9a87681b
Nguyễn Danh Nam <izero.cs@gmail.com>
2023-12-25 21:40:48 +0700

    Update rename

--- frontend/src/components/explorer/view/List.tsx
+++ frontend/src/components/explorer/view/List.tsx
@@ -2,7 +2,7 @@ import classNames from "classnames"
 import React, { useState, useEffect } from "react"
 import FileModel, { FileAdapter } from "../model/File"
 import { OnDirectoryClickCallback, OnFileClickCallback } from "../"
-import { useAppDispatch, useAppSelector } from "../../../store/Hooks"
+import { useAppSelector } from "../../../store/Hooks"
 import { ContextMenuSelector } from "../../../store/reducers/ContextMenu"
 import { OnContextMenuClickCallback } from "../View"
 
@@ -22,55 +22,78 @@ type ExplorerViewListProps = {
   onContextMenuClick: OnContextMenuClickCallback
 }
 
-const ExplorerViewList: React.FC<
-  ExplorerViewListProps & React.HTMLAttributes<HTMLDivElement>
-> = (props) => {
+const ExplorerViewList: React.FC<ExplorerViewListProps & React.HTMLAttributes<HTMLDivElement>> = (
+  props
+) => {
   const isContextMenuShow = useAppSelector(ContextMenuSelector.isMenuShow)
   const [rowActived, setRowActived] = useState(-1)
   const [cellActived, setCellActived] = useState(-1)
+  const [cellNameFocus, setCellNameFocus] = useState(false)
 
-  let timeoutID: any = null
+  let cellNameElement: any = null
+  let cellNameInput: any = null
+  let cellNameTime: number = -1
+
+  const dispatchCellNameActived = (targetElement: any) => {
+    cellNameInput = Object.values(targetElement.children).find((child: any) =>
+      child.getAttribute("data-cell-input")
+    )
+
+    cellNameElement = targetElement
+    cellNameTime = Date.now()
+    setCellNameFocus(false)
+  }
+
+  const dispatchCellNameFocusNow = () => {
+    if (cellNameInput && cellNameInput.tagName === "INPUT") {
+      cellNameInput.setSelectionRange(cellNameInput.value.length, cellNameInput.value.length)
+      cellNameInput.focus()
+      setCellNameFocus(true)
+
+      setCellActived(parseInt(cellNameElement.getAttribute("data-cell-input")))
+    }
+  }
+
+  const dispatchCellNameFocusInterval = () => {
+    if (cellNameFocus === false && cellNameTime !== -1 && Date.now() - cellNameTime > 1000) {
+      dispatchCellNameFocusNow()
+    }
+  }
+
+  const dispatchCellNameUnfocus = () => {
+    cellNameInput = null
+    cellNameElement = null
+    cellNameTime = -1
+
+    setCellNameFocus(false)
+  }
+
+  const onRowKeyDown = (event: any) => {
+    const keycode = event.keyCode
+
+    // console.log(keycode)
+  }
 
   const onRowClick = (event: any, fileModel: FileModel, index: number): any => {
     const targetElement = event.target
-    let parentElement = targetElement.parentElement
 
     if (
       targetElement &&
       rowActived === index &&
       targetElement.getAttribute("data-cell-input") !== null
     ) {
-      timeoutID = setTimeout(() => {
-        console.log("Func timeout")
-        const input: any = Object.values(targetElement.children).find(
-          (child: any) => child.getAttribute("data-cell-input")
-        )
-
-        setCellActived(parseInt(targetElement.getAttribute("data-cell-input")))
-
-        if (input && input.tagName === "INPUT") {
-          input.setSelectionRange(input.value.length, input.value.length)
-          input.focus()
-        }
-      }, 2000)
+      console.log("Cell Row actived")
+      dispatchCellNameActived(targetElement)
     } else {
       setCellActived(-1)
     }
 
-    if (
-      parentElement &&
-      parentElement.getAttribute("data-row-index") === null
-    ) {
-      parentElement = parentElement.parentElement
-    }
-
-    if (rowActived !== index && timeoutID) {
-      console.log("Clear timeout id")
-      timeoutID = null
-      clearTimeout(timeoutID)
+    if (rowActived !== index) {
+      dispatchCellNameUnfocus()
     }
 
     setRowActived(index)
+    window.addEventListener("keydown", onRowKeyDown)
 
     if (!isContextMenuShow) {
       event.preventDefault()
@@ -80,11 +103,7 @@ const ExplorerViewList: React.FC<
     }
   }
 
-  const onRowDoubleClick = (
-    event: any,
-    fileModel: FileModel,
-    index: number
-  ): any => {
+  const onRowDoubleClick = (event: any, fileModel: FileModel, index: number): any => {
     let pass = false
 
     if (props.onDirectoryClick && fileModel.is_directory) {
@@ -96,22 +115,18 @@ const ExplorerViewList: React.FC<
     event.preventDefault()
 
     if (pass) {
+      dispatchCellNameUnfocus()
       setRowActived(-1)
       setCellActived(-1)
       event.stopPropagation()
-      clearTimeout(timeoutID)
-
-      timeoutID = null
+      window.removeEventListener("keydown", onRowKeyDown)
     }
 
     return false
   }
 
-  const onRowContextMenu = (
-    event: React.MouseEvent,
-    fileModel: FileModel,
-    index: number
-  ): any => {
+  const onRowContextMenu = (event: any, fileModel: FileModel, index: number): any => {
+    dispatchCellNameUnfocus()
     setRowActived(index)
     event.preventDefault()
     event.stopPropagation()
@@ -121,21 +136,41 @@ const ExplorerViewList: React.FC<
   }
 
   const onWindowOutside = (event: any) => {
-    setRowActived(-1)
-    setCellActived(-1)
+    let pass = false
+
+    if (event.target) {
+      let element = event.target
+      let rowIndex = element.getAttribute("data-row-index")
+      let cellInput = element.getAttribute("data-cell-input")
+
+      if (cellInput !== null) {
+        rowIndex = null
+        pass = true
+      } else if (rowIndex === null) {
+        element = event.target.parentElement
+        rowIndex = element.getAttribute("data-row-index")
+      }
+
+      if (rowIndex !== null && parseInt(rowIndex) === rowActived) {
+        pass = true
+      }
+    }
+
+    if (!pass) {
+      setRowActived(-1)
+      setCellActived(-1)
+    }
   }
 
   const fileModels = props.fileModels || []
   const filterColumns = props.filterColumns.filter(
     (col) => typeof col.show === "undefined" || col.show === true
   )
-  const hasColumnName =
-    filterColumns.find((col) => col.isName === true) || filterColumns[0]
+  const hasColumnName = filterColumns.find((col) => col.isName === true) || filterColumns[0]
   const hasColumnStretch = filterColumns.find((col) => col.size === "stretch")
 
   const colSize = (col: FilterColumn, index: number): string => {
-    if ((!hasColumnStretch && index === 0) || "stretch" === col.size)
-      return "stretch"
+    if ((!hasColumnStretch && index === 0) || "stretch" === col.size) return "stretch"
     return col.size || "small"
   }
 
@@ -156,12 +191,15 @@ const ExplorerViewList: React.FC<
   })
 
   useEffect(() => {
+    const cellNameInterval = setInterval(dispatchCellNameFocusInterval, 50)
+
     window.addEventListener("click", onWindowOutside)
     window.addEventListener("contextmenu", onWindowOutside)
 
     return () => {
       window.removeEventListener("click", onWindowOutside)
       window.removeEventListener("contextmenu", onWindowOutside)
+      clearInterval(cellNameInterval)
     }
   })
 
@@ -174,10 +212,7 @@ const ExplorerViewList: React.FC<
               return (
                 <td
                   key={index}
-                  className={classNames(
-                    "explorer-view-list-head-cell",
-                    colSize(col, index)
-                  )}
+                  className={classNames("explorer-view-list-head-cell", colSize(col, index))}
                 >
                   <span className="label">{col.label}</span>
                 </td>
@@ -199,12 +234,10 @@ const ExplorerViewList: React.FC<
                 data-row-index={index}
               >
                 {filterColumns.map((col, filterIndex) => {
-                  const operation =
-                    FileAdapter[col.key as keyof typeof FileAdapter]
+                  const operation = FileAdapter[col.key as keyof typeof FileAdapter]
                   let value = item[col.key as keyof FileModel] as string
 
-                  if (typeof operation !== "undefined")
-                    value = operation(value, item)
+                  if (typeof operation !== "undefined") value = operation(value, item)
 
                   return (
                     <td
@@ -213,7 +246,7 @@ const ExplorerViewList: React.FC<
                         "explorer-view-list-body-cell",
                         colSize(col, filterIndex)
                       )}
-                      {...(col == hasColumnName && {
+                      {...(col === hasColumnName && {
                         "data-cell-input": filterIndex
                       })}
                     >
@@ -234,7 +267,8 @@ const ExplorerViewList: React.FC<
                           className={classNames("input-label", {
                             "input-actived":
                               index === rowActived &&
-                              filterIndex === cellActived
+                              filterIndex === cellActived &&
+                              cellNameFocus === true
                           })}
                           value={value}
                           title={value}
